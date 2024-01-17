df882da127d788fab95ed27a4b7bacf43b0117f3
Jimmy <Jimmy@DESKTOP-4LPTNLM>
2024-01-17 16:22:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace studenten_voortgang_applicatie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
